0b20cfce4d6b5ff029006d467131d7c375cf8240
AGaleea <a_galeeva@hotmail.com>
2024-03-20 15:46:33 +0100

    structure created

--- src/main/java/de/aittr/team24_FP_backend/domain/User.java
+++ src/main/java/de/aittr/team24_FP_backend/domain/User.java
@@ -4,7 +4,7 @@ import jakarta.persistence.*;
 
 @Entity
 @Table(name = "user")
-public class User {
+public class User implements de.aittr.team24_FP_backend.domain.interfaces.User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -22,4 +22,66 @@ public class User {
 
     @Column(name = "lastName")
     private String lastName;
+
+    public User() {
+    }
+
+    public User(int id, String email, String password, String firstName, String lastName) {
+        this.id = id;
+        this.email = email;
+        this.password = password;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    @Override
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    @Override
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return "User: id = %d, firstName = %s, lastName = %s, email = %s".formatted(id, firstName, lastName, email);
+//        return String.format("User: id = %d, firstName = %s, lastName = %s", email = %s, id, firstName, lastName, email);
+    }
 }
---
+++ src/main/java/de/aittr/team24_FP_backend/services/UserService.java
@@ -0,0 +1,7 @@
+package de.aittr.team24_FP_backend.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+}
